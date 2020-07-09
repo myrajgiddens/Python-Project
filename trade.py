ff5ae87432397d7b5228478f69fd8617492839b6
Myra <myrajgiddens@aol.com>
2020-07-09 13:00:50 -0400

    added base url

--- trade.py
+++ trade.py
@@ -8,7 +8,9 @@ SECRET_KEY = "iL88NatBdu8oZumuNwmGLzkPGkk9eAYayXt6ztkR"
 
 BASE_URL = "https://paper-api.alpaca.markets"
 
-ACCOUNT_URL= "{}/v2/account".format(BASE_URL)
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 
 r = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
 
