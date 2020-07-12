bc70f20f56870ccb60847b132079da1c765ef8df
Myra <myrajgiddens@aol.com>
2020-07-12 14:18:15 -0400

    adding in react

--- trade.py
+++ trade.py
@@ -11,7 +11,7 @@ ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
 def get_account():
-    r = requests.get(ACCOUNT_URL, headers = HEADERS)
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
     return json.loads(r.content)
 
 def create_order(symbol, qty, side, type, time_in_force):
@@ -21,13 +21,17 @@ def create_order(symbol, qty, side, type, time_in_force):
         "side": side,
         "type": type,
         "time_in_force": time_in_force
-
-
     }
+
     r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
 
     return json.loads(r.content)
 
-response = create_order("AAPL", 100, "buy", "market", "gtc")
+def get_orders():
+    r = requests.get(ORDERS_URL, headers=HEADERS)
 
-print(response)
+    return json.loads(r.content)
+    
+orders = get_orders()
+
+print(orders)
